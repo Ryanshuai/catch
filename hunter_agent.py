b31f0e96ca2542d1f2c61179777e55d56010dfc6
Ryan <1018718155@qq.com>
2017-06-14 17:49:57 +0800

    修改了一内容
    构造函数基本完成

--- hunter_agent.py
+++ hunter_agent.py
@@ -15,6 +15,10 @@ tf.set_random_seed(1)
 class DeepQNetwork:
     def __init__(self):
         self.n_actions = 4 #up down left right
+        self.n_robot = 4
+        self.memory_counter = 0
+        self.memory = np.zeros((self.memory_size,84*84*4))  # initialize zero memory [s, a, r, s_]
+        self.learn_step_counter = 0  # total learning step
 
         self.batch_size = 32
         self.memory_size = 1000000  # replay memory size
@@ -26,12 +30,13 @@ class DeepQNetwork:
         self.initial_exploration = 1
         self.final_exploration = 0.1
         self.final_exploration_frame = 1000000
-        self.reply_start_size = 50000
+        self.replay_start_size = 50000
         #used by RMSProp
         self.learning_rate = 0.00025
         self.gredient_momentum = 0.95
         self.squared_gredient_momentum = 0.95
         self.min_squared_gradient = 0.01
+
 
         self._build_net()# consist of [target_net, evaluate_net]
         self.sess = tf.Session()
@@ -42,10 +47,7 @@ class DeepQNetwork:
     def _build_net(self):
 
         # ------------------ build evaluate_net ------------------
-        im = tf.placeholder(tf.float32, shape=[None, 84, 84, 4]) / 255  # [10,224,224,3]
-        x = tf.placeholder(tf.float32, shape=[None, 1])
-        y = tf.placeholder(tf.float32, shape=[None, 1])
-        # i_s = tf.placeholder(tf.float32, shape=[1])
+        self.im_to_evaluate_net = tf.placeholder(tf.float32, shape=[None, 84, 84, 4]) / 255  # [10,84,84,4]
 
         keep_prob = tf.placeholder(tf.float32)
 
@@ -53,7 +55,7 @@ class DeepQNetwork:
         ## conv1 layer ##
         W_conv1 = tf.Variable(tf.truncated_normal([8, 8, 4, 32], stddev=0.1),collections = col_eval_net)
         b_conv1 = tf.Variable(tf.constant(0.1, shape=[32]),collections = col_eval_net)
-        conv1 = tf.nn.conv2d(im, W_conv1, strides=[1, 4, 4, 1], padding='SAME')
+        conv1 = tf.nn.conv2d(self.im_to_evaluate_net, W_conv1, strides=[1, 4, 4, 1], padding='SAME')
         h_conv1 = tf.nn.relu(conv1 + b_conv1)
 
         ## conv2 layer ##
@@ -68,8 +70,8 @@ class DeepQNetwork:
         conv3 = tf.nn.conv2d(h_conv2, W_conv3, strides=[1, 1, 1, 1], padding='SAME')
         h_conv3 = tf.nn.relu(conv3 + b_conv3)
 
-        # [n_samples, 6, 6, 64] ->> [n_samples, 2304]
-        h_pool3_flat = tf.reshape(h_conv3, [-1, 2304])
+        # [n_samples, 6, 6, 64] ->> [n_samples, 7744]
+        h_pool3_flat = tf.reshape(h_conv3, [-1, 7744])
 
         ## fc4 layer ##
         W_fc4 = tf.Variable(tf.truncated_normal([9216, 4096], stddev=0.1),collections = col_eval_net)
@@ -78,19 +80,16 @@ class DeepQNetwork:
         h_fc4_drop = tf.nn.dropout(h_fc4, keep_prob)
 
         ## fc5 layer ##
-        W_fc5 = tf.Variable(tf.truncated_normal([4096, 512], stddev=0.1),collections = col_eval_net)
-        b_fc5 = tf.Variable(tf.constant(0.1, shape=[512]),collections = col_eval_net)
+        W_fc5 = tf.Variable(tf.truncated_normal([4096, self.n_actions*self.n_robot], stddev=0.1),collections = col_eval_net)
+        b_fc5 = tf.Variable(tf.constant(0.1, shape=[self.n_actions*self.n_robot]),collections = col_eval_net)
         h_fc5 = tf.matmul(h_fc4_drop, W_fc5) + b_fc5
-        self.q_eval = h_fc5
+        self.q_eval = h_fc5 #所有action的Q组成一个tensor
 
         self.loss = tf.reduce_mean(tf.squared_difference(self.q_target, self.q_eval))
         self._train_op = tf.train.RMSPropOptimizer(self.learning_rate,).minimize(self.loss)???
 
         # ------------------ build target_net ------------------
-        im = tf.placeholder(tf.float32, shape=[None, 84, 84, 4]) / 255  # [10,224,224,3]
-        x = tf.placeholder(tf.float32, shape=[None, 1])
-        y = tf.placeholder(tf.float32, shape=[None, 1])
-        # i_s = tf.placeholder(tf.float32, shape=[1])
+        self.im_to_target_net = tf.placeholder(tf.float32, shape=[None, 84, 84, 4]) / 255  # [10,84,84,4]
 
         keep_prob = tf.placeholder(tf.float32)
 
@@ -98,7 +97,7 @@ class DeepQNetwork:
         ## conv1 layer ##
         W_conv1 = tf.Variable(tf.truncated_normal([8, 8, 4, 32], stddev=0.1),collections = col_targ_net)
         b_conv1 = tf.Variable(tf.constant(0.1, shape=[32]),collections = col_targ_net)
-        conv1 = tf.nn.conv2d(im, W_conv1, strides=[1, 4, 4, 1], padding='SAME')
+        conv1 = tf.nn.conv2d(self.im_to_target_net, W_conv1, strides=[1, 4, 4, 1], padding='SAME')
         h_conv1 = tf.nn.relu(conv1 + b_conv1)
 
         ## conv2 layer ##
@@ -113,8 +112,8 @@ class DeepQNetwork:
         conv3 = tf.nn.conv2d(h_conv2, W_conv3, strides=[1, 1, 1, 1], padding='SAME')
         h_conv3 = tf.nn.relu(conv3 + b_conv3)
 
-        # [n_samples, 6, 6, 64] ->> [n_samples, 2304]
-        h_pool3_flat = tf.reshape(h_conv3, [-1, 2304])
+        # [n_samples, 6, 6, 64] ->> [n_samples, 7744]
+        h_pool3_flat = tf.reshape(h_conv3, [-1, 7744])
 
         ## fc4 layer ##
         W_fc4 = tf.Variable(tf.truncated_normal([9216, 4096], stddev=0.1),collections = col_targ_net)
@@ -123,13 +122,13 @@ class DeepQNetwork:
         h_fc4_drop = tf.nn.dropout(h_fc4, keep_prob)
 
         ## fc5 layer ##
-        W_fc5 = tf.Variable(tf.truncated_normal([4096, 512], stddev=0.1),collections = col_targ_net)
-        b_fc5 = tf.Variable(tf.constant(0.1, shape=[512]),collections = col_targ_net)
+        W_fc5 = tf.Variable(tf.truncated_normal([4096, self.n_actions*self.n_robot], stddev=0.1),collections = col_targ_net)
+        b_fc5 = tf.Variable(tf.constant(0.1, shape=[self.n_actions*self.n_robot]),collections = col_targ_net)
         h_fc5 = tf.matmul(h_fc4_drop, W_fc5) + b_fc5
         self.q_next = h_fc5
 
-    def store_transition(self, s, a, r, s_):
-        transition = np.hstack((s, [a, r], s_)) #horizontally stack
+    def store_transition(self, fi, a, r, fi_):
+        transition = np.hstack((fi, [a, r], fi_)) #horizontally stack
 
         index = self.memory_counter % self.memory_size # replace the old memory with new memory
         self.memory[index, :] = transition #override the old memory
@@ -142,21 +141,19 @@ class DeepQNetwork:
 
         if np.random.uniform() < self.epsilon: #mao si fan le
             # forward feed the observation and get q value for every actions
-            actions_value = self.sess.run(self.q_eval, feed_dict={self.s: observation})
+            actions_value = self.sess.run(self.q_eval, feed_dict={self.: observation})
             action = np.argmax(actions_value)
         else:
             action = np.random.randint(0, self.n_actions)
         return action
 
-    def _replace_target_params(self):
-        t_params = tf.get_collection('target_net_params')
-        e_params = tf.get_collection('eval_net_params')
-        self.sess.run([tf.assign(t, e) for t, e in zip(t_params, e_params)])
 
     def learn(self):
-        # check to reeplace target parameters
-        if self.learn_step_counter % self.replace_target_iter == 0:
-            self._replace_target_params()
+        # check to replace target parameters
+        if self.learn_step_counter % self.target_network_update_frequency == 0:
+            t_params = tf.get_collection('target_net_params')
+            e_params = tf.get_collection('eval_net_params')
+            self.sess.run([tf.assign(t, e) for t, e in zip(t_params, e_params)])
             print('target_params_rplaced')
 
         # sample batch memory from all memory
@@ -182,27 +179,6 @@ class DeepQNetwork:
 
         q_target[batch_index, eval_act_index] = reward + self.gamma * np.max(q_next, axis=1)
 
-        """
-        For example in this batch I have 2 samples and 3 actions:
-        q_eval =
-        [[1, 2, 3],
-         [4, 5, 6]]
-        q_target = q_eval =
-        [[1, 2, 3],
-         [4, 5, 6]]
-        Then change q_target with the real q_target value w.r.t the q_eval's action.
-        For example in:
-            sample 0, I took action 0, and the max q_target value is -1;
-            sample 1, I took action 2, and the max q_target value is -2:
-        q_target =
-        [[-1, 2, 3],
-         [4, 5, -2]]
-        So the (q_target - q_eval) becomes:
-        [[(-1)-(1), 0, 0],
-         [0, 0, (-2)-(6)]]
-        We then backpropagate this error w.r.t the corresponding action to network,
-        leave other action as error=0 cause we didn't choose it.
-        """
 
         # train eval network
         _, self.cost = self.sess.run([self._train_op, self.loss],
